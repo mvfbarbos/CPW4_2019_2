578361d4fd958ce0cc310bab3b4eb022c5b2035c
“Marcus” <mvfbarbos@msn.com>
2019-11-20 22:49:41 -0400

    Estilizando a págnia de visualização da série

--- tv-search-pwa/src/pages/VisualizarSerie.js
+++ tv-search-pwa/src/pages/VisualizarSerie.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
 
 import logo from "../img/paginaAnterior.png";
-
 import Header from "../components/Header";
+import "./VisualizarSerie.scss";
 
 export default class VisualizarSerie extends Component {
 
     render() {
+
         const { serie, imagem } = this.props.location.state;
         const dados = serie.show;
-
-
+        console.log(dados);
 
         let generos = dados.genres[0];
-        const quantidadeGeneros= dados.genres.length;
-        if(quantidadeGeneros.length >1) {
-            for(let i=1;i<quantidadeGeneros;i++) {
-                generos += `,dados,genres[i]`;
+        const quantidadeGeneros = dados.genres.length;
+        if (quantidadeGeneros.length > 1) {
+            for (let i = 1; i < quantidadeGeneros; i++) {
+                generos += `,${dados.genres[i]}`;
             }
         }
+
+        const titulo = dados.name;
+
+        /**
+         * Expressão regular para
+         * remover tags da string
+         */
+        const regex = /(<([^>]+)>)/ig;
+        const lingua = dados.language;
+
+        /*let canalTV=";
+        if(dados.network){
+            canalTV=dados.network.name;
+        } */
+        const canalTV = dados.network ? dados.network.name : ""; // : é o else         
+        const canalWeb = dados.webChannel ? dados.webChannel.name : "";
+        const { status } = dados;
+        const sinopse = dados.summary.replace(regex, "");
 
         return (
             <div>
@@ -30,10 +48,44 @@ export default class VisualizarSerie extends Component {
                 <div id="areaCartaz">
                     <img src={imagem} alt="Cartaz" />
                 </div>
-                <div id="areaDados" >
-                    <h2>{dados.nome}</h2>
-                    
-                    <span id="generos">{generos}</span>
+
+                <div id="areaDadosSerie" >
+                    <h2>{titulo}</h2>
+
+                    <span className="nomeCampo">Gênero: </span>
+                    <span>{generos}</span>
+                    <br />
+
+
+                    <span className="nomeCampo">Língua: </span>
+                    <span>{lingua}</span>
+                    <br />
+
+                    {
+                        canalTV &&
+                        <div>
+                            <span className="nomeCampo">Canal de TV:</span>
+                            <span>{canalTV}</span>
+                            <br />
+                        </div>
+                    }
+
+                    {
+                        canalWeb &&
+                        <div>
+                            <span className="nomeCampo">Streaming:</span>
+                            <span>{canalWeb}</span>
+                            <br />
+                        </div>
+                    }
+
+                    <span className="nomeCampo">Status:</span>
+                    <span id="status">{status}</span>
+                    <br />
+
+
+                    <span className="nomeCampo">Sinopse:</span>
+                    <div id="sinopse">{sinopse}</div>
                 </div>
 
 
